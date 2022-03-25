0a83362a5d96d09e82e9a1157fd212b6858e0123
shruti0419 <shrutimohanty2019@gmail.com>
2022-03-25 11:40:37 +0530

    login signup

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import Main from "./components/Main";
+import Signup from "./components/Singup";
+import Login from "./components/Login";
+
+function App() {
+	const user = localStorage.getItem("token");
+
+	return (
+		<Routes>
+			<Route path="/main" exact element={<Main />} />
+			<Route path="/signup" exact element={<Signup />} />
+			<Route path="/login" exact element={<Login />} />
+		</Routes>
+	);
+}
+
+export default App;
